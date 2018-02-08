24d07ea9a7cbba8a38cb0438004bfbff93f9a72a
Xavier Bourry <xavier.bourry@polytechnique.org>
2018-02-08 17:12:36 +0100

    remove deploy script

--- deploy/deployIntegration.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo 'Deploying integration demos'
-scp -r ../integrationDemo ../integrationDemo2 ../integrationDemoYoutube jeeliz@spacegoo.com:/home/jeeliz/www/demos/glanceTracker/
-echo 'Demos deployed!'
